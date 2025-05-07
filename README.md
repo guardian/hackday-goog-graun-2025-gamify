16218cb5529cbf06b9a12cd58aa5dfb18d939628
Liam Duffy <3338808+liamness@users.noreply.github.com>
2025-05-07 16:33:05 +0100

    Followed ADK tutorial steps

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ Hacky hack hack
 
 # Getting started
 
+## Starting the UI:
+
 `mise use node`
 `npm i`
 `npm run dev`
+
+## Starting the Gemini agent server
+
+Set up the `gcloud` CLI and run `gcloud auth login`, then do:
+
+`mise use python`
+`pip install adk`
+`adk run agent`
+
+Look [in docs](https://google.github.io/adk-docs/get-started/quickstart/) if that doesn't work (I probably forgot something):
---
+++ agent/agent.py
@@ -0,0 +1,14 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+root_agent = Agent(
+    name="test_agent",
+    model="gemini-2.5-pro-preview-05-06",
+    description=(
+        "Agent to lookup articles on theguardian.com and provide an investigative experience for the Panama papers"
+    ),
+    instruction=(
+        "You are a helpful agent who can search theguardian.com using the 'google_search' tool."
+    ),
+    tools=[google_search],
+)
